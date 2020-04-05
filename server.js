8eeccf09b83a7aeff771f0f31453b0334d5ed040
zhou467778 <zhou.wann@husky.neu.edu>
2020-04-04 21:03:25 -0400

    edited app.listen in server.js, trying to fix heroku issue

--- server.js
+++ server.js
@@ -1,30 +1,19 @@
 const express = require('express')
 const app = express()
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers',
-//         'Content-Type, X-Requested-With, Origin');
-//     res.header('Access-Control-Allow-Methods',
-//         'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-//     next();
-// });
-app.use(function(req, res, next) {
-    const allowedOrigins = ['http://myherokuapp.com', 'http://localhost:4200', 'http://127.0.0.1:9000', 'http://localhost:9000'];
-    const origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
 });
+
 
 require('./controllers/quiz.controller.server')(app);
 require('./controllers/question.controller.server')(app);
 
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
 
